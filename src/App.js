7dee58b7e0cf52a494da4b1c149aa031681707a5
FLORIN RINJA <FRINJA@mapal-os.com>
2026-02-17 14:32:43 +0100

    updated some components;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPageScroller from 'react-page-scroller';
 import './App.css';
 import About from './components/About';
@@ -7,12 +7,49 @@ import ExpEdu from './components/ExpEdu';
 import WorksOne from './components/WorksOne';
 import WorksTwo from './components/WorksTwo';
 import WorksThree from './components/WorksThree';
+import { Pagination } from 'react-bootstrap';
 
 const App = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageChange = number => {
+    if(number === currentPage) return;
+    setCurrentPage(number);
+  };
+
+  const handleBeforePageChange = page => {
+    if(page === currentPage) return;
+    setCurrentPage(page);
+  };
+
+  const getPagesNumbers = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= 6; i++) {      
+      pageNumbers.push(
+        <Pagination.Item 
+          key={i} 
+          active={(i - 1) === currentPage} 
+          activeLabel=""
+          className="florin-rinja-page-item"
+          onClick={() => handlePageChange(i-1, 'getPagesNumbers')}
+        >
+          {i}
+        </Pagination.Item>,
+      );
+    }
+
+    return [...pageNumbers];
+  };
+  
+  const pagesNumbers = getPagesNumbers();
 
   return (
-    <div className='appJs' >
-      <ReactPageScroller >
+    <React.Fragment>
+      <ReactPageScroller
+        onBeforePageScroll={handleBeforePageChange}
+        customPageNumber={currentPage}
+      >
         <About />
         <Competences />
         <WorksOne />
@@ -20,8 +57,11 @@ const App = () => {
         <WorksThree />
         <ExpEdu />
       </ReactPageScroller>
-    </div>
+      <Pagination className="pagination-additional-class" size="lg">
+        {pagesNumbers}
+      </Pagination>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/components/Competences.jsx
+++ src/components/Competences.jsx
@@ -3,76 +3,73 @@ import './Competences.css';
 import { Row } from 'react-bootstrap';
 import Phaser from './images/phaser-logo.svg';
 
-const Competences = () => {
-    return (
-        <div className='container-fluid competences'>
-            <Row className='col-xs-12 col-sm-6 col-m-6 col-lg-6'>
-                <div className='vertical-skills'>
-                    skills
-                </div>
-            </Row>
-            <Row className='col-xs-12 col-sm-6 col-m-6 col-lg-6'>
-                <div className='centeredIcons'>
-                    <ul class="list-inline list-icons">
-                        <li class="list-inline-item">
-                            <i class="devicon-html5-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-css3-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-bootstrap-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-javascript-plain colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-react-original-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-nodejs-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-mysql-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-mongodb-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <img src={Phaser} alt='phaser' />
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-linux-plain colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-ubuntu-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-git-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-github-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-visualstudio-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-travis-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-trello-plain-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-express-original-wordmark colored"></i>
-                        </li>
-                        <li class="list-inline-item">
-                            <i class="devicon-d3js-plain colored"></i>
-                        </li>
-                    </ul>
-                </div>
-                <h2 className='boldSkills'>skills</h2>
-            </Row>
-        </div>
-    )
-}
-export default Competences;
+export default () => ((
+		<div className='container-fluid competences'>
+			<Row className='col-xs-12 col-sm-6 col-m-6 col-lg-6'>
+				<div className='vertical-skills'>
+					skills
+				</div>
+			</Row>
+			<Row className='col-xs-12 col-sm-6 col-m-6 col-lg-6'>
+				<div className='centeredIcons'>
+					<ul className="list-inline list-icons">
+						<li className="list-inline-item">
+							<i className="devicon-html5-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-css3-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-bootstrap-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-javascript-plain colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-react-original-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-nodejs-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-mysql-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-mongodb-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<img src={Phaser} alt='phaser' />
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-linux-plain colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-ubuntu-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-git-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-github-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-visualstudio-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-travis-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-trello-plain-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-express-original-wordmark colored"></i>
+						</li>
+						<li className="list-inline-item">
+							<i className="devicon-d3js-plain colored"></i>
+						</li>
+					</ul>
+				</div>
+				<h2 className='boldSkills'>skills</h2>
+			</Row>
+		</div>
+	));
